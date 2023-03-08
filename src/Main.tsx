373a84be98876b352e84fb26b6e9955e275bada4
youngcheolLee <zodiac0905@gmail.com>
2023-03-08 20:55:49 +0900

    React router 페이지 구조 및 설정

---
+++ src/Main.tsx
@@ -0,0 +1,11 @@
+import { Router } from "./Router";
+
+export const Main = () => {
+  return (
+    <div className="Main">
+      <p>추후 HEADER 영역</p>
+      <Router />
+      <p>추후 FOOTER 영역</p>
+    </div>
+  );
+};
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,11 @@
+import axios from "axios";
+
+export const HomePage = () => {
+  // const searchOption = async () => {
+  //   const optionResponse = await axios.get(
+  //     "https://developer-lostark.game.onstove.com/markets/opttions",{headers: }
+  //   );
+  // };
+
+  return <div>Home Page!</div>;
+};
